c50abfd6cbc7d27d662a2a0c661d5fe569dd58ca
Maria Jan Matejka <mq@jmq.cz>
2020-11-10 21:53:04 +0000

    Fixed BSD build

--- sysdep/unix/alloc.c
+++ sysdep/unix/alloc.c
@@ -18,7 +18,9 @@
 
 #ifdef HAVE_MMAP
 static u64 page_size = 0;
+#ifdef HAVE_ALIGNED_ALLOC
 static _Bool use_fake = 0;
+#endif
 #else
 static const u64 page_size = 4096; /* Fake page size */
 #endif
@@ -33,9 +35,13 @@ u64 get_page_size(void)
   {
     if ((u64_popcount(page_size) > 1) || (page_size > 16384))
     {
+#ifdef HAVE_ALIGNED_ALLOC
       /* Too big or strange page, use the aligned allocator instead */
       page_size = 4096;
       use_fake = 1;
+#else
+      bug("Strange page size: %lu", page_size);
+#endif
     }
     return page_size;
   }
@@ -48,33 +54,45 @@ void *
 alloc_page(void)
 {
 #ifdef HAVE_MMAP
+#ifdef HAVE_ALIGNED_ALLOC
   if (!use_fake)
   {
+#endif
     void *ret = mmap(NULL, get_page_size(), PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (ret == MAP_FAILED)
       bug("mmap(%lu) failed: %m", page_size);
     return ret;
+#ifdef HAVE_ALIGNED_ALLOC
   }
   else
 #endif
+#endif
+#ifdef HAVE_ALIGNED_ALLOC
   {
     void *ret = aligned_alloc(page_size, page_size);
     if (!ret)
       bug("aligned_alloc(%lu) failed", page_size);
     return ret;
   }
+#endif
 }
 
 void
 free_page(void *ptr)
 {
 #ifdef HAVE_MMAP
+#ifdef HAVE_ALIGNED_ALLOC
   if (!use_fake)
   {
+#endif
     if (munmap(ptr, get_page_size()) < 0)
       bug("munmap(%p) failed: %m", ptr);
+#ifdef HAVE_ALIGNED_ALLOC
   }
   else
 #endif
+#endif
+#ifdef HAVE_ALIGNED_ALLOC
     free(ptr);
+#endif
 }
